704c86e05d2bbb9cc4dc90656a35282aa3ec43a0
Saeid Hosseinabadi <saeed221373@yahoo.com>
2025-11-30 21:33:47 +0330

    feat: Replace AtbIcon SVG with Linea-Luck.png image

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,13 +7,8 @@ export function LineaIcon(props: Omit<React.ComponentProps<typeof Image>, 'src' 
     )
 }
 
-export function AtbIcon(props: SVGProps<SVGSVGElement>) {
+export function AtbIcon(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-            <circle cx="12" cy="12" r="10" />
-            <path d="m14.31 8 5.74 9.94" />
-            <path d="m9.69 8-5.74 9.94" />
-            <path d="M2 12h20" />
-        </svg>
+        <Image src="/assets/Linea-Luck.png" alt="Linea Luck" {...props} />
     )
 }
